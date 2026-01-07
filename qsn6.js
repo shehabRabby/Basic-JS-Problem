d2f961c32467da0ffe1d09877692087615d2c9d1
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2026-01-08 01:02:07 +0600

    6 an 7 added

---
+++ qsn6.js
@@ -0,0 +1,10 @@
+function sum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+const numbers = [1, 2, 3, 4];
+console.log(sum(numbers))
---
+++ qsn7.js
@@ -0,0 +1,5 @@
+
+const getEvenNumbers = arr => {
+    return arr.filter(num => num % 2 === 0);
+}
+console.log(getEvenNumbers([1, 2, 3, 4, 5, 6])); 
